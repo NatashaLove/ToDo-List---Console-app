c231a229cc7242fff6285be00a4e9201d656d5db
Natasha S Love <debora@inbox.ru>
2018-09-19 18:22:54 -0500

    something

--- ToDo/ToDo.java
+++ ToDo/ToDo.java
@@ -2,9 +2,23 @@ package ToDo;
 
 public class ToDo {
 
-public static int count=0;
+	public static int count=0;
+	String item;
 
-
+	void addToDo (String item){
+		this.item=item;
+	}
+	boolean markDone (){
+		 
+	}
+	
+	int getCount () {
+		return count;
+	}
+	
+	String getToDo () {
+		return 
+	}
 
 
 
--- ToDoList.java
+++ ToDoList.java
@@ -5,6 +5,8 @@ public class  ToDoList {
 	
 	public static void main (String [] args) {
 	
+	//ArrayList<String> al= new ArrayList<String>();
+	ArrayList<ToDo> todo = new ArrayList<ToDo>();
 	
 	
 	
